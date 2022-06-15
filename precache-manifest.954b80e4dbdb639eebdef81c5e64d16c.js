ef1f80e6db606f4a24a1b9bafee74a3dde2bf1bc
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2022-06-15 22:44:38 +0530

    Updates

---
+++ precache-manifest.954b80e4dbdb639eebdef81c5e64d16c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b28c2094d4c8fc7b3474cf8b61078",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a2e036f5e6d63d215f9",
+    "url": "./static/css/main.42fac1f0.chunk.css"
+  },
+  {
+    "revision": "b62459edf11683b91fc2",
+    "url": "./static/js/2.af7fd837.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.af7fd837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2e036f5e6d63d215f9",
+    "url": "./static/js/main.a6ebbc6e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.a6ebbc6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd2a13e6a1f4a385d70",
+    "url": "./static/js/runtime-main.b044ddb6.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/toast.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "3f249626faaed407caebe25cc16b51d2",
+    "url": "./static/media/toast.3f249626.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/toast.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/toast.c0d3c94c.eot"
+  }
+]);
